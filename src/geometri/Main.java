44e25150167f50ab9cef9bb0a61f38b25cb22983
AkhmadNizarr <nizarbjb87@gmail.com>
2023-06-13 09:30:55 +0700

    Membuat class main dan menjalankan programnya

---
+++ src/geometri/Main.java
@@ -0,0 +1,16 @@
+
+package geometri;
+
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Lingkaran nizar = new Lingkaran(2,4);
+        PersegiPanjang nizar = new PersegiPanjang(2,4);
+       
+        
+    }
+    
+    
+    
+}
